8fe121745218d5945a39b08b45b70ca4ce40ebc6
VinaySehwag14 <vinaysehwag14@gmail.com>
2021-11-25 02:11:56 +0530

    POST PAGE

--- src/component/post/Post.jsx
+++ src/component/post/Post.jsx
@@ -1,42 +1,29 @@
 import "./post.css";
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://images.pexels.com/photos/9190648/pexels-photo-9190648.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-        alt=""
-      />
+      {post.photo && (
+        <img
+          className="postImg"
+          src="https://images.pexels.com/photos/9190648/pexels-photo-9190648.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt=""
+        />
+      )}
+
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">
-          Lorem ipsum dolor sit amet sds ciunt .
-        </span>
+        <span className="postTitle">{post.title}</span>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium
-        ullam neque voluptates voluptate dolorem debitis, laboriosam quibusdam
-        ea, temporibus quam dolor quos nostrum, dicta vero? Ipsam aliquid Lorem
-        ipsum dolor sit amet consectetur, adipisicing elit. Laudantium ullam
-        neque voluptates voluptate dolorem debitis, laboriosam quibusdam ea,
-        temporibus quam dolor quos nostrum, dicta vero? Ipsam aliquid Lorem
-        ipsum dolor sit amet consectetur, adipisicing elit. Laudantium ullam
-        neque voluptates voluptate dolorem debitis, laboriosam quibusdam ea,
-        temporibus quam dolor quos nostrum, dicta vero? Ipsam aliquid Lorem
-        ipsum dolor sit amet consectetur, adipisicing elit. Laudantium ullam
-        neque voluptates voluptate dolorem debitis, laboriosam quibusdam ea,
-        temporibus quam dolor quos nostrum, dicta vero? Ipsam aliquid Lorem
-        ipsum dolor sit amet consectetur, adipisicing elit. Laudantium ullam
-        neque voluptates voluptate dolorem debitis, laboriosam quibusdam ea,
-        temporibus quam dolor quos nostrum, dicta vero? Ipsam aliquid mollitia
-        ad at.
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,27 @@ import Posts from "../../component/posts/Posts";
 import Sidebar from "../../component/sidebar/Sidebar";
 import Header from "../../component/header/Header";
 import "./home.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API } from "../../backend";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get(`${API}/post`);
+      //*comment for checking post
+      console.log(res);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
