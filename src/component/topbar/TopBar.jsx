60130e2cab6f44c93f3cd7865cac7b6033e396ff
VinaySehwag14 <55314386+VinaySehwag14@users.noreply.github.com>
2023-04-18 17:07:38 +0530

    Update TopBar.jsx
    updated code

--- src/component/topbar/TopBar.jsx
+++ src/component/topbar/TopBar.jsx
@@ -84,7 +84,9 @@ const TopBar = () => {
               className="topImage"
               src={PF + user.profilePic}
               alt="Authorimage"
-            /> : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Favatar-icon-placeholder-facebook-1577909%2F&psig=AOvVaw0aDIMEO2FTrE_ouNhXRErh&ust=1681903269569000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNj0wYmos_4CFQAAAAAdAAAAABAE"}
+            /> : <img className="topImage" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Favatar-icon-placeholder-facebook-1577909%2F&psig=AOvVaw0aDIMEO2FTrE_ouNhXRErh&ust=1681903269569000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNj0wYmos_4CFQAAAAAdAAAAABAE"
+      alt="Author" />
+}
           </Link>
         ) : (
           <ul className="topList">
